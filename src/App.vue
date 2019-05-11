616ee901e161a5bea68ab8484638097d32268dfc
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-11 22:03:27 +0200

    add h1 and 2h in template Arrow.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Arrow></Arrow>
 </template>
 
 <script>
+import Arrow from './components/Arrow/Arrow.vue';
 
 export default {
   name: 'App',
   components: {
+    Arrow, 
   },
   data () {
     return {
---
+++ src/components/Arrow/Arrow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav>
+    </nav>
+    <header>
+      <h1>Bracaval Elias</h1>
+      <h2>Developer Full Stack Junior</h2>
+    </header>
+    <nav>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'Arrow'
+  }
+</script>
+
+<style scoped>
+
+</style>
+
