dbf3fbc4736bc91c95526b22fdadd71637947148
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-20 21:26:31 +0200

    copy portfolio

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.elias-bracaval.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:yatoogamii/yatoogamii.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yatoogamii/Portfolio.git master:gh-pages
+# git push -f git@github.com:yatoogamii/Portfolio.git master:gh-pages
 
 cd -
