5700d09287138619c98a92cf5a2cbe557cf7a5ce
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-03 18:34:31 +0200

    add TextAbout component

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -1,19 +1,19 @@
 <template>
   <section id="about" class="about">
     <Img></Img>
-    <Text></Text>
+    <TextAbout></TextAbout>
   </section>
 </template>
 
 <script>
 import Img from './Img.vue';
-import Text from './Text.vue';
+import TextAbout from './TextAbout.vue';
 
   export default{
     name: 'About',
     components: {
       Img,
-      Text
+      TextAbout
     }
   }
 </script>
@@ -22,6 +22,9 @@ import Text from './Text.vue';
 .about {
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 }
 </style>
 
---
+++ src/components/About/TextAbout.vue
@@ -0,0 +1,34 @@
+<template>
+  <article class="about__text-container">
+    <h1>Apprenant professionnelle</h1>
+    <p>Amet repellat reiciendis doloribus accusantium saepe similique? Impedit quisquam pariatur aspernatur eveniet assumenda Itaque laboriosam itaque odit aliquam odit aliquid? Commodi omnis a minus modi deleniti quos laboriosam voluptates! Quo dicta aspernatur iusto officia incidunt eaque. Natus expedita odio vitae reiciendis natus Libero repellat dignissimos sequi aperiam perspiciatis quas quod Hic modi officia enim quidem inventore. Quam eos dicta ut libero dolor, deleniti. Commodi molestias fuga consectetur minima distinctio commodi vero Aliquid pariatur rerum delectus exercitationem cumque dolores. Atque sint praesentium totam repudiandae laudantium Laborum consequatur quo sapiente ut rem? Ut tempore id quam nobis a asperiores. Sit quo sunt</p>
+    <p>The harder i work, the luckier i get</p>
+  </article>
+</template>
+
+<script>
+  export default{
+    name: 'TextAbout', 
+  }
+</script>
+
+<style lang="scss" scoped>
+
+.about__text-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  color: rgba(255, 255, 255, 1);
+  font-size: 2rem;
+  padding: 5rem;
+  width: 50%;
+  height: 90%;
+
+  h1 {
+
+  }
+}
+
+</style>
+
