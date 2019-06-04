e3777566388b5409eb14db9e5d8da91939bcc2cc
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-05 00:21:09 +0200

    add base html in project.vue

---
+++ src/components/Projects/Project.vue
@@ -0,0 +1,29 @@
+<template>
+    <article :style="width">
+      <div class="container-project">
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/2b/37/3c/ocean-blue-tenerife.jpg" alt="preview site"/>
+      </div>
+    </article>
+</template>
+
+<script>
+export default{
+  name: 'Project',
+  props: ['width']
+}
+</script>
+
+<style lang="scss" scoped>
+
+.container-project {
+  background-color: red;
+  width: 50%;
+}
+
+img {
+  width: 100%;
+  height: 50%;
+}
+
+</style>
+
--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,12 +1,21 @@
 <template>
   <section id="project" class="project">
-
+    <Project width="width: 40%"></Project>
+    <Project width="width: 40%"></Project>
+    <Project width="width: 33.33%"></Project>
+    <Project width="width: 33.33%"></Project>
+    <Project width="width: 33.33%"></Project>
   </section>
 </template>
 
 <script>
+import Project from './Project.vue';
+
   export default{
-    
+    name: 'Projects',
+    components: {
+      Project
+    }
   }
 </script>
 
@@ -14,6 +23,10 @@
 .project {
   width: 100vw;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 
 </style>
