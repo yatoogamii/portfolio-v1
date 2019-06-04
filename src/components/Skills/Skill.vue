1d028cd32e8f9852cb5d77e5a1c983cbfe839a52
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-04 03:57:57 +0200

    add skill component and setup props

---
+++ src/components/Skills/Skill.vue
@@ -0,0 +1,35 @@
+<template>
+  <article :style="width" class="skills__skill">
+    <v-progress-circular
+      :rotate="360"
+      :size="150"
+      :width="20"
+      :value="value"
+      :color="color"
+      >
+      {{ this.text }}
+    </v-progress-circular>
+
+  </article>
+</template>
+
+<script>
+export default{
+  name: 'Skill',
+  props: ['value', 'color', 'text', 'width']
+}
+</script>
+
+<style lang="scss" scoped>
+
+.skills__skill {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  height: 33.33%;
+}
+
+
+</style>
+
--- src/components/Skills/Skills.vue
+++ src/components/Skills/Skills.vue
@@ -1,21 +1,36 @@
 <template>
   <section id="skills" class="skills">
-    
+    <Skill color="blue" value="90" text="html" width="width: 34%"></Skill>
+    <Skill color="green" value="70" text="Js" width="width: 34%"></Skill>
+    <Skill color="blue" value="90" text="html" width="width: 33.33%"></Skill>
+    <Skill color="red" value="50" text="css" width="width: 33.33%"></Skill>
+    <Skill color="green" value="70" text="Js" width="width: 33.33%"></Skill>
+    <Skill color="blue" value="90" text="html" width="width: 33.33%"></Skill>
+    <Skill color="red" value="50" text="css" width="width: 33.33%"></Skill>
+    <Skill color="green" value="70" text="Js" width="width: 33.33%"></Skill>
   </section>
 </template>
 
 <script>
+import Skill from './Skill.vue';
+
   export default{
-    
+    name: 'Skills',
+    components: {
+      Skill
+    }
   }
 </script>
 
 <style scoped>
+
 .skills {
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
-
 
 </style>
 
