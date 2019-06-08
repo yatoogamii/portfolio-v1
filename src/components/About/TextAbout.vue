e14c4a5b6f54e0e89fd3728e4f0638cc1bc2be07
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-09 00:56:15 +0200

    update text in about section

--- src/components/About/TextAbout.vue
+++ src/components/About/TextAbout.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="about__text-container">
     <h1 class="about__text-title">Apprenant <span class="about__text--red">professionnel</span></h1>
-    <p class="about__text-desc">Je me présente, Elias Bracaval, 19 ans. Je suis sur un pc depuis l'âge de <span class="about__text--red">6 ans</span>, j'ai d'abord découvert le code en <span class="about__text--red">autodidacte</span> avant de me lancer dans une formation de <span class="about__text--red">développer Web</span>. Je suis actuellement en perpétuel recherche de <span class="about__text--red">nouveauté</span> dans le domaine </p>
+    <p class="about__text-desc">Je me présente, Elias Bracaval, 19 ans. Je suis sur un pc depuis l'âge de <span class="about__text--red">6 ans</span>, j'ai d'abord découvert le code en <span class="about__text--red">autodidacte</span> avant de me lancer dans une formation de <span class="about__text--red">développer Web</span>. Je suis en perpétuel recherche de <span class="about__text--red">nouveauté</span> dans le domaine </p>
     <p class="about__text-quote">The <span class="about__text--red">harder</span> i work, the <span class="about__text--red">luckier</span> i get</p>
   </article>
 </template>
