537e8a59f66d9864d19878f337a55ecef845906f
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-09 13:57:44 +0200

    finish animation skill

--- src/components/Navbar/Header.vue
+++ src/components/Navbar/Header.vue
@@ -23,7 +23,8 @@ export default{
           top : "13vw",
           color : "#1d1d1d",
         }
-      }
+      },
+      toogleBool: true
     };
   },
   methods: {
@@ -55,9 +56,41 @@ export default{
       } else {
         this.anchor.link = "#about";
       }
-    }
-  },
 
+      this.animeSkills();
+    },
+    animeSkills() {
+      if ( this.toogleBool && this.anchor.link == '#skills') {
+        this.toogleBool = false;
+        setTimeout(() => { 
+          const github = document.getElementsByClassName('github')[0].ldBar;
+          const css = document.getElementsByClassName('css')[0].ldBar;
+          const linux = document.getElementsByClassName('linux')[0].ldBar;
+          const vim = document.getElementsByClassName('vim')[0].ldBar;
+          const react = document.getElementsByClassName('react')[0].ldBar;
+          const vuejs = document.getElementsByClassName('vuejs')[0].ldBar;
+          const postgresql = document.getElementsByClassName('postgresql')[0].ldBar;
+          const js = document.getElementsByClassName('js')[0].ldBar;
+          github.set(0)
+          github.set(70)
+          css.set(0)
+          css.set(80)
+          linux.set(0)
+          linux.set(70)
+          vim.set(0)
+          vim.set(80)
+          react.set(0)
+          react.set(50)
+          vuejs.set(0)
+          vuejs.set(60)
+          js.set(0)
+          js.set(70)
+          postgresql.set(0)
+          postgresql.set(60)
+        }, 700);
+      }
+    }
+  }
 }
 
 </script>
--- src/components/Skills/Skill.vue
+++ src/components/Skills/Skill.vue
@@ -3,16 +3,16 @@
     <v-tooltip bottom>
       <template v-slot:activator="{ on }">
         <div :class="allClass"
-             data-type="fill"
-             data-fill-dir="ltr"
-             :data-img="img"
-             :data-value="pourcent"
-             data-img-size="125,125"
-             style="margin:auto"
-             v-on="on"
-             data-fill-background-extrude="1"
-             data-fill-background="#3D3D3D"
-             ></div>
+                data-type="fill"
+                data-fill-dir="ltr"
+                :data-img="img"
+                :data-value="pourcent"
+                data-img-size="125,125"
+                style="margin:auto"
+                v-on="on"
+                data-fill-background-extrude="1"
+                data-fill-background="#3D3D3D"
+                ></div>
       </template>
       <span> 
         <h1>{{ title }}</h1>
@@ -29,38 +29,9 @@ export default{
   data() {
     return {
       allClass: "ldBar " + this.theme,
-      pourcent: 0
+      pourcent: 0,
     };
   },
-  mounted() {
-    setTimeout(() => { 
-      const github = document.getElementsByClassName('github')[0].ldBar;
-      const css = document.getElementsByClassName('css')[0].ldBar;
-      const linux = document.getElementsByClassName('linux')[0].ldBar;
-      const vim = document.getElementsByClassName('vim')[0].ldBar;
-      const react = document.getElementsByClassName('react')[0].ldBar;
-      const vuejs = document.getElementsByClassName('vuejs')[0].ldBar;
-      const postgresql = document.getElementsByClassName('postgresql')[0].ldBar;
-      const js = document.getElementsByClassName('js')[0].ldBar;
-      github.set(0)
-      github.set(70)
-      css.set(0)
-      css.set(80)
-      linux.set(0)
-      linux.set(70)
-      vim.set(0)
-      vim.set(80)
-      react.set(0)
-      react.set(50)
-      vuejs.set(0)
-      vuejs.set(60)
-      js.set(0)
-      js.set(70)
-      postgresql.set(0)
-      postgresql.set(60)
-    }, 700);
-  }
-  
 }
 
 </script>
