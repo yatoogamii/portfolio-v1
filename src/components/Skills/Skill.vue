262c473df9b65167b0d5fd168f4fd2cf889448a6
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-09 13:26:21 +0200

    add animation skill

--- src/components/Skills/Skill.vue
+++ src/components/Skills/Skill.vue
@@ -2,11 +2,11 @@
   <article :style="width" class="skills__skill">
     <v-tooltip bottom>
       <template v-slot:activator="{ on }">
-        <div class="ldBar"
+        <div :class="allClass"
              data-type="fill"
              data-fill-dir="ltr"
              :data-img="img"
-             :data-value="value"
+             :data-value="pourcent"
              data-img-size="125,125"
              style="margin:auto"
              v-on="on"
@@ -25,7 +25,42 @@
 <script>
 export default{
   name: 'Skill',
-  props: ['value', 'width', 'img', 'title', 'tooltip'],
+  props: ['value', 'width', 'img', 'title', 'tooltip', 'theme'],
+  data() {
+    return {
+      allClass: "ldBar " + this.theme,
+      pourcent: 0
+    };
+  },
+  mounted() {
+    setTimeout(() => { 
+      const github = document.getElementsByClassName('github')[0].ldBar;
+      const css = document.getElementsByClassName('css')[0].ldBar;
+      const linux = document.getElementsByClassName('linux')[0].ldBar;
+      const vim = document.getElementsByClassName('vim')[0].ldBar;
+      const react = document.getElementsByClassName('react')[0].ldBar;
+      const vuejs = document.getElementsByClassName('vuejs')[0].ldBar;
+      const postgresql = document.getElementsByClassName('postgresql')[0].ldBar;
+      const js = document.getElementsByClassName('js')[0].ldBar;
+      github.set(0)
+      github.set(70)
+      css.set(0)
+      css.set(80)
+      linux.set(0)
+      linux.set(70)
+      vim.set(0)
+      vim.set(80)
+      react.set(0)
+      react.set(50)
+      vuejs.set(0)
+      vuejs.set(60)
+      js.set(0)
+      js.set(70)
+      postgresql.set(0)
+      postgresql.set(60)
+    }, 700);
+  }
+  
 }
 
 </script>
--- src/components/Skills/Skills.vue
+++ src/components/Skills/Skills.vue
@@ -1,20 +1,20 @@
 <template>
   <section id="skills" class="skills">
-    <Skill title="Github" tooltip="Commit, PR" img="./github.svg" value="70" width="width: 34%">
+    <Skill theme="github" title="Github" tooltip="Commit, PR" img="./github.svg" value="70" width="width: 34%">
     </Skill>
-    <Skill title="CSS" tooltip="Flexbox, Grid, SASS"  img="./css.svg" value="80" width="width: 34%">
+    <Skill theme="css" title="CSS" tooltip="Flexbox, Grid, SASS"  img="./css.svg" value="80" width="width: 34%">
     </Skill>
-    <Skill title="Linux" tooltip="Manjaro, Ubuntu"  img="./linux.svg" value="70" width="width: 33.33%">
+    <Skill theme="linux" title="Linux" tooltip="Manjaro, Ubuntu"  img="./linux.svg" value="70" width="width: 33.33%">
     </Skill>
-    <Skill title="VueJS" tooltip="Vue CLI, Nuxt.js, Vuetify" img="./vuejs.svg" value="60" width="width: 33.33%">
+    <Skill theme="vuejs" title="VueJS" tooltip="Vue CLI, Nuxt.js, Vuetify" img="./vuejs.svg" value="60" width="width: 33.33%">
     </Skill>
-    <Skill title="ReactJS" tooltip="RCA, Reactify" img="./react.svg" value="50" width="width: 33.33%">
+    <Skill theme="react" title="ReactJS" tooltip="RCA, Reactify" img="./react.svg" value="50" width="width: 33.33%">
     </Skill>
-    <Skill title="Vim" tooltip="Plugin, Macro" img="./vim.svg" value="80" width="width: 33.33%">
+    <Skill theme="vim" title="Vim" tooltip="Plugin, Macro" img="./vim.svg" value="80" width="width: 33.33%">
     </Skill>
-    <Skill title="JS/Node.js" tooltip="ES6+, Express, Mongoose, Sequelize" img="./nodejs.svg" value="70" width="width: 33.33%">
+    <Skill theme="js" title="JS/Node.js" tooltip="ES6+, Express, Mongoose, Sequelize" img="./nodejs.svg" value="70" width="width: 33.33%">
     </Skill>
-    <Skill title="PostgreSQL" tooltip="Jointure" img="./postgresql.svg" value="60" width="width: 33.33%">
+    <Skill theme="postgresql" title="PostgreSQL" tooltip="Jointure" img="./postgresql.svg" value="60" width="width: 33.33%">
     </Skill>
   </section>
 </template>
